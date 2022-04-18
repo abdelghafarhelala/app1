994a840ca7deefcd69500b72923344e7ae429254
abdelghafar <abdelghafarhelala@gmail.com>
2022-04-18 16:21:12 +0200

    add words to language file

--- lib/modules/serviceScreen/serviceScreen.dart
+++ lib/modules/serviceScreen/serviceScreen.dart
@@ -1,5 +1,4 @@
 import 'package:app1/app_locale/app_locale.dart';
-import 'package:app1/shared/colors.dart';
 import 'package:flutter/material.dart';
 
 class ServiceScreen extends StatelessWidget {
@@ -79,6 +78,7 @@ class ServiceScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Image.network(
                   'https://img.freepik.com/free-vector/flat-customer-support-illustration_23-2148899114.jpg?t=st=1650286763~exp=1650287363~hmac=244f771ec1effc2b72dfd83c519903aef8bc743141ef3cd98d795f7029171852&w=740',
@@ -90,16 +90,16 @@ class ServiceScreen extends StatelessWidget {
                   height: 20,
                 ),
                 Text(
-                  'Title of service',
+                  '${getLang(context, 'titleOfService')}',
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
                 const SizedBox(
                   height: 10,
                 ),
                 Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                  '${getLang(context, 'lorem')}',
                   style: Theme.of(context).textTheme.caption,
-                  maxLines: 2,
+                  maxLines: 3,
                   overflow: TextOverflow.ellipsis,
                 ),
               ],
